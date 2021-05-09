adb8dfd2b9d1c386cc4811a30a6ee3735dcd1210
adeesha <a3adeeshaanjana@gmail.com>
2021-05-09 12:04:22 +0530

    commit mistake

--- cal.py
+++ cal.py
@@ -1,25 +1,25 @@
-# def addition(num1,num2):
-#     return num1 + num2
-# def subraction(num1,num2):
-#     return num1 - num2
-# def division(num1,num2):
-#     return num1 / num2
-# def multiplication(num1,num2):
-#     return num1 * num2
+def addition(num1,num2):
+    return num1 + num2
+def subraction(num1,num2):
+    return num1 - num2
+def division(num1,num2):
+    return num1 / num2
+def multiplication(num1,num2):
+    return num1 * num2
 
-# while True:
-#     num1 = float(input("Enter first number:"))
-#     choice = input("Enter operation:")
-#     num2 = float(input("Enter second number:"))
+while True:
+    num1 = float(input("Enter first number:"))
+    choice = input("Enter operation:")
+    num2 = float(input("Enter second number:"))
     
-#     if choice in ('+','-','*','/'):
-#         # num1 = float(input("Enter first number:"))
+    if choice in ('+','-','*','/'):
+        # num1 = float(input("Enter first number:"))
 
-#         if choice == '+':
-#             print(num1,"+",num2,"=", addition(num1,num2))
-#         elif choice == '-':
-#             print(num1,"-",num2,"=", subraction(num1,num2))
-#         elif choice == '/':
-#             print(num1,"/",num2,"=", division(num1,num2))
-#         elif choice == '*':
-#             print(num1,"*",num2,"=", multiplication(num1,num2))
+        if choice == '+':
+            print(num1,"+",num2,"=", addition(num1,num2))
+        elif choice == '-':
+            print(num1,"-",num2,"=", subraction(num1,num2))
+        elif choice == '/':
+            print(num1,"/",num2,"=", division(num1,num2))
+        elif choice == '*':
+            print(num1,"*",num2,"=", multiplication(num1,num2))
